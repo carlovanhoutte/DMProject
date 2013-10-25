d5eadd69b6b3a98e2e3336a201510899cb735ac6
TheMBc <maarten.bruggeman@outlook.com>
2013-10-25 10:41:49 +0200

    Lukt commit-pull?

---
+++ src/Ticket.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Maarten
+ * Date: 25/10/13
+ * Time: 10:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class Ticket {
+
+}
