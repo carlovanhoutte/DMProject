ab3fb800d78c0a2f89c215c76be0b281a95185be
Carlo Vanhoutte <carlo.vanhoutte@gmail.com>
2013-10-25 10:15:23 +0200

    Init

---
+++ src/Festival.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Carlo
+ * Date: 25/10/13
+ * Time: 10:14
+ * To change this template use File | Settings | File Templates.
+ */
+public class Festival {
+}
